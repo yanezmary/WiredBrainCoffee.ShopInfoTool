19153789360f226ad9979178586dcd74366f4e0e
MARY YANEZ <yanezmary@hotmail.com>
2020-01-05 17:57:37 +0100

    Add Program

---
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        }
+    }
+}
