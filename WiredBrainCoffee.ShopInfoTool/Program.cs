88464a3416d2d07424edf509d5e7e4d31311dcdc
Alexandra <yanezmary@hotmail.com>
2020-01-05 20:36:09 +0100

    Introduce command handlers

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -19,43 +19,20 @@ namespace WiredBrainCoffee.ShopInfoTool
             while (true)
             {
                 var line = Console.ReadLine();
-                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
-                if(string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    if(string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
-                    {
-                        break;
-                    }
-                    Console.WriteLine("> Available coffee shop commands:");
-                    foreach(var coffeeShop in coffeeShops)
-                    {
-                        Console.WriteLine($"> " + coffeeShop.Location);
-                    }
-                }
-                else
-                {
-                    var foundCoffeeShops = coffeeShops.Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase)).
-                        ToList();
 
-                    if(foundCoffeeShops.Count == 0)
-                    {
-                        Console.WriteLine($" > Command '{line}' not found");
-                    }
-                    else if(foundCoffeeShops.Count == 1)
-                    {
-                        var coffeeShop = foundCoffeeShops.Single();
-                        Console.WriteLine($"> Location: {coffeeShop.Location}");
-                        Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} kg ");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"> Multiple matching coffee shop commands found:");
-                        foreach(var coffeeType in foundCoffeeShops)
-                        {
-                            Console.WriteLine($"> {coffeeType.Location}");
-                        }
-                    }
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
                 }
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                ICommandHandler commandHandler =
+                string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+                ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+                : new CoffeeShopCommandHandler(coffeeShops, line);
+
+                commandHandler.HandleCommand();
             }
 
         }
